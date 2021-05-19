44ef8471b44b3b7f6aef55d5188d7aad7d8fb069
Fabio <fabio.p@tuta.io>
2021-05-19 17:30:21 +0200

    Inserite le Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-/* import './App.css'; */
+/* import logo from './logo.svg'; */
+import './App.css';
+import VideoText from './VideoText';
+
+
 
 function App() {
+  // PROPS - propietà, dati che passiamo
   return (
     <div className="App">
-        <h1>Ciao REACT</h1>
+        <VideoText title="Four Season Vivaldi" views="230mld" published="10 years ago" user="Fabio Grandi" />
+        <VideoText title="The best of Vivaldi" views="33mld" published="7 years ago" user="Giuseppe Rossi" />
+        <VideoText title="Antonio Vivaldi la stravaganza" views="17mld" published="8 years ago" user="Mario Bianchi" />
+        <VideoText title="Antonio Vivaldi: le quattro stagioni" views="4mld" published="13 years ago" user="Antonio Pozzi" />
+        <VideoText title="Vivaldi - Complete Violin Concertos" views="1mld" published="3 years ago" user="Giorgia Meluzzi" />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/VideoText.js
@@ -0,0 +1,19 @@
+
+
+function VideoText(props) {
+    return (
+        <div style={{ display: "flex" }}>
+            <div style= {{ padding: '30px', background: 'red' }} >
+                <h1>Image</h1>
+            </div>
+            <div>
+                <h1> Titolo: {props.title} </h1>
+                <h4> Visualizzazioni: {props.views} Pubblicato: {props.published} </h4>
+                <h2> User: {props.user} </h2>
+            </div>
+        </div>
+    )
+}
+
+export default VideoText;
+
