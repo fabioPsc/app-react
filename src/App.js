14733bb4207bdcd014d67494d9c1251857dca745
Fabio <fabio.p@tuta.io>
2021-05-19 17:48:39 +0200

    Inserito dello stile per cambiare i colori

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
   // PROPS - propietà, dati che passiamo
   return (
     <div className="App">
-        <VideoText title="Four Season Vivaldi" views="230mld" published="10 years ago" user="Fabio Grandi" />
-        <VideoText title="The best of Vivaldi" views="33mld" published="7 years ago" user="Giuseppe Rossi" />
-        <VideoText title="Antonio Vivaldi la stravaganza" views="17mld" published="8 years ago" user="Mario Bianchi" />
-        <VideoText title="Antonio Vivaldi: le quattro stagioni" views="4mld" published="13 years ago" user="Antonio Pozzi" />
-        <VideoText title="Vivaldi - Complete Violin Concertos" views="1mld" published="3 years ago" user="Giorgia Meluzzi" />
+        <VideoText title="Four Season Vivaldi" views="230mld" published="10 years ago" user="Fabio Grandi" color="blue" />
+        <VideoText title="The best of Vivaldi" views="33mld" published="7 years ago" user="Giuseppe Rossi" color="green" />
+        <VideoText title="Antonio Vivaldi la stravaganza" views="17mld" published="8 years ago" user="Mario Bianchi" color="white" />
+        <VideoText title="Antonio Vivaldi: le quattro stagioni" views="4mld" published="13 years ago" user="Antonio Pozzi" color="violet" />
+        <VideoText title="Vivaldi - Complete Violin Concertos" views="1mld" published="3 years ago" user="Giorgia Meluzzi" color="red" />
     </div>
   );
 }
--- src/VideoText.js
+++ src/VideoText.js
@@ -3,11 +3,11 @@
 function VideoText(props) {
     return (
         <div style={{ display: "flex" }}>
-            <div style= {{ padding: '30px', background: 'red' }} >
+            <div style= {{ padding: '30px', background: props.color }} >
                 <h1>Image</h1>
             </div>
             <div>
-                <h1> Titolo: {props.title} </h1>
+                <h1 style= {{ color: 'blue' }} > Titolo: {props.title} </h1>
                 <h4> Visualizzazioni: {props.views} Pubblicato: {props.published} </h4>
                 <h2> User: {props.user} </h2>
             </div>
@@ -16,4 +16,6 @@ function VideoText(props) {
 }
 
 export default VideoText;
+
+
 
