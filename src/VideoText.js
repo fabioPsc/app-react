32f405ab2de48756dc6b011b331c0241a81be90f
Fabio <fabio.p@tuta.io>
2021-05-19 18:30:51 +0200

    inserito del codice in VideoText per semplificare

--- src/VideoText.js
+++ src/VideoText.js
@@ -1,15 +1,19 @@
 
 
 function VideoText(props) {
+
+    // DESTRUCTURING 
+    /* console.log(props); */
+    const {title, views, published, user, color} = props;
     return (
         <div style={{ display: "flex" }}>
-            <div style= {{ padding: '30px', background: props.color }} >
+            <div style= {{ padding: '30px', background: color }} >
                 <h1>Image</h1>
             </div>
             <div>
-                <h1 style= {{ color: 'blue' }} > Titolo: {props.title} </h1>
-                <h4> Visualizzazioni: {props.views} Pubblicato: {props.published} </h4>
-                <h2> User: {props.user} </h2>
+                <h1 style= {{ color: 'blue' }} > Titolo: {title} </h1>
+                <h4> Visualizzazioni: {views} Pubblicato: {published} </h4>
+                <h2> User: {user} </h2>
             </div>
         </div>
     )
