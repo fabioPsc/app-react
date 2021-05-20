40b5e9356d9f891c9764a135d5b5bac9b3fa3a3c
Fabio <fabio.p@tuta.io>
2021-05-20 23:29:55 +0200

    cambiato il testo con un numero che va ad incrementarsi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 /* import logo from './logo.svg'; */
 import './App.css';
 /* import VideoText from './VideoText'; */
+//CONCETTO DI STATE 
 import {useState} from 'react';
 
 function App() {
   
-  const [nome, setNome] = useState('Billy')
-  const cambiaNome = () => {
-    setNome('Johnny')
-    /* console.log(nome) */
+  const [numero, setNumero] = useState(0)
+  const aumentaNumero = () => {
+    setNumero(numero + 7)
+    console.log(numero)
   }
   
 return (
   <div className="App">
   
-    <button onClick= {cambiaNome} >Cambia Nome</button>
-      <h3>Ciao {nome} </h3>
+    <button onClick= {aumentaNumero} >Cambia Nome</button>
+      <h3>Ciao {numero} </h3>
   </div>
   );
   
