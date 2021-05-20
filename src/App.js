54757e4f6761b44d6d970004bbde288709928b72
Fabio <fabio.p@tuta.io>
2021-05-20 10:35:20 +0200

    Aggiunta funzione e bottone eventi

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import VideoText from './VideoText';
 
 
 function App() {
-  // PROPS - propietà, dati che passiamo
+  
+  const saluta = () => {
+    console.log('ciao mondo')
+  }
+
   return (
     <div className="App">
+      {/* Aggiungere eventi */}
+      <button onClick={saluta}>Click me</button>
         <VideoText title="Four Season Vivaldi" views="230mld" published="10 years ago" user="Fabio Grandi" color="blue" />
-        <VideoText title="The best of Vivaldi" views="33mld" published="7 years ago" user="Giuseppe Rossi" color="green" />
-        <VideoText title="Antonio Vivaldi la stravaganza" views="17mld" published="8 years ago" user="Mario Bianchi" color="white" />
-        <VideoText title="Antonio Vivaldi: le quattro stagioni" views="4mld" published="13 years ago" user="Antonio Pozzi" color="violet" />
+        <VideoText title="The best of Vivaldi" views="33mld" published="7 years ago" user="Giuseppe Rossi" color="violet" />
+        <VideoText title="Antonio Vivaldi la stravaganza" views="17mld" published="8 years ago" user="Mario Bianchi" color="green" />
+        <VideoText title="Antonio Vivaldi: le quattro stagioni" views="4mld" published="13 years ago" user="Antonio Pozzi" color="white" />
         <VideoText title="Vivaldi - Complete Violin Concertos" views="1mld" published="3 years ago" user="Giorgia Meluzzi" color="red" />
     </div>
   );
