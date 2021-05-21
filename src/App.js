7b27af50ee64e48838842ea55b1c1c9d2960551d
Fabio <fabio.p@tuta.io>
2021-05-21 14:12:57 +0200

    inserita la View e installata estensione

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 /* import VideoText from './VideoText'; */
 //CONCETTO DI STATE 
 import {useState} from 'react';
+import VideoText from './VideoText';
 
 function App() {
   const [text, setText] = useState("")
@@ -14,6 +15,8 @@ function App() {
   
   return (
     <div className="App">
+      <VideoText title="video di Vivaldi" views="3 mld" published="2 years Ago" user="Fabio Piscini" color="red"/>
+      <VideoText title="Le 4 Stagioni di Vivaldi" views="33 mld" published="7 years Ago" user="Claudio tremaglia" color="green"/>
       <input type="text" onChange={onChangeHandler} value={text} />
       {text}
       <button onClick={()=>setText("")} >Azzera Test</button>
